02fc396d5b50d630cb6a57aec1164683310fc821
Adi Kaplan <adikaplan@MacBook-Air-sl-Adi.local>
2025-12-25 20:34:44 +0200

    completed

--- Koch.java
+++ Koch.java
@@ -8,7 +8,11 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+		/* 
+        StdDraw.setCanvasSize(500, 500);
+		StdDraw.setXscale(0, 500);
+        StdDraw.setYscale(0, 500);
+
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
@@ -17,18 +21,37 @@ public class Koch {
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		*/
 
-		/*
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
+		if( n == 0){
+			StdDraw.setPenColor(StdDraw.BLACK);
+			StdDraw.line(x1, y1, x2, y2);
+		return;
+	}
+	    StdDraw.setPenColor(StdDraw.BLACK);
 		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		double p3X = (Math.sqrt(3)/6.0)*(y1 - y2) + 0.5 * (x1 + x2);
+		double p3Y = (Math.sqrt(3)/6.0)*(x2 - x1) + 0.5 * (y1 + y2);
+		double p4X = (2*x1 + x2) /3.0;
+		double p4Y = (2*y1 +y2) /3.0;
+		double p5X = (x1 + 2*x2) /3.0;
+		double p5Y = (y1 + 2*y2) /3.0;
+		StdDraw.line(p4X, p4Y, p3X, p3Y);
+		StdDraw.line(p5X, p5Y, p3X, p3Y);
+		StdDraw.setPenColor(StdDraw.WHITE);
+		StdDraw.line(p4X, p4Y, p5X, p5Y);
+		curve(n-1, x1, y1, p4X, p4Y);
+		curve(n-1,p4X, p4Y, p3X, p3Y);
+		curve(n-1,p3X, p3Y, p5X, p5Y);
+		curve(n-1,p5X, p5Y, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -37,6 +60,15 @@ public class Koch {
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.2;
+		double y1 = 0.8;
+		double x2 = 0.8;
+		double y2 = 0.8;
+		double x3 = 0.5;
+		double height = 0.6 * Math.sqrt(3) / 2.0;
+		double y3 = 0.8 - height;
+		curve(n, x1, y1, x2, y2);
+		curve(n, x2, y2, x3, y3);
+		curve(n, x3, y3, x1, y1);
 	}
 }
--- Sierpinski.java
+++ Sierpinski.java
@@ -2,17 +2,43 @@
 public class Sierpinski {
 	
 	public static void main(String[] args) {
+		StdDraw.setCanvasSize(700, 700);
+		StdDraw.setXscale(0, 700);
+        StdDraw.setYscale(0, 700);
 		sierpinski(Integer.parseInt(args[0]));
+			 
 	}
 	
 	// Draws a Sierpinski triangle of depth n on the standard canvass.
 	public static void sierpinski (int n) {
-		//// Replace this comment with your code
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.line(50, 50, 570, 50);
+		StdDraw.line(50, 50, 310, 500);
+		StdDraw.line(570, 50, 310, 500);
+
+	sierpinski(n, 50, 310, 570, 50, 500, 50);
 	}
-	
+
 	// Does the actual drawing, recursively.
 	private static void sierpinski(int n, double x1, double x2, double x3,
 		                                 double y1, double y2, double y3) {
-		//// Replace this comment with your code
+
+	if( n == 0){
+		return;
+	}
+	double cordinateX1 = x2;
+	double cordinateY1 = y1;
+	double cordinateX2 = (x1 + x2) /2;
+	double cordinateY2 = (y2 + y1)/2;
+	double cordinateX3 = (x3 + x2) /2;
+	double cordinateY3 = (y2 + y1)/2;
+	//StdDraw.setPenColor(StdDraw.BLACK);
+	StdDraw.line(cordinateX1, cordinateY1, cordinateX3, cordinateY3);
+	StdDraw.line(cordinateX1, cordinateY1, cordinateX2, cordinateY2);
+	StdDraw.line(cordinateX3, cordinateY3, cordinateX2, cordinateY2);
+	sierpinski(n-1, x1, cordinateX2,cordinateX1, y1, cordinateY2, cordinateY1);
+	sierpinski(n-1, cordinateX2, x2, cordinateX3, cordinateY2, y2, cordinateY3);
+	sierpinski(n-1, cordinateX1, cordinateX3, x3, cordinateY1,  cordinateY3, y3);
+
 	}
 }
